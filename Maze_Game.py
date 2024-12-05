b62f2beb7f6b5f30c0fb90594da4bcbf7b9d365c
TONn618 <162407058+TONn618@users.noreply.github.com>
2024-12-05 20:43:02 +0900

    Update Maze_Game.py

--- Maze_Game.py
+++ Maze_Game.py
@@ -8,8 +8,24 @@ class OutsideBoundaryError(Exception):
     """외벽에서 이동하려고 할 경우 발생하는 사용자 정의 예외"""
     def __init__(self, message):
         super().__init__(message)
+        
+class WidthSizeError(Exception):
+    """미로 가로 크기가 지정 범위 (20 ~ 70) 을 벗어날 때 발생하는 사용자 정의 예외"""
+    def __init__(self, message):
+        super().__init__(message)
+
+class HeightSizeError(Exception):
+    """미로 세로 크기가 지ㅓ범위 (10 ~ 60) 을 벗어날 때 발생하는 사용자 정의 예외"""
+    def __init__(self, message):
+        super().__init__(message)
+
+class InvalidValueError(Exception):
+    """입력받은 미로 크기가 정수형이 아닐 때 발생하는 사용자 정이 예외"""
+    def __init__(self, message):
+        super().__init__(message)
 
 
+    
 class Maze:
     def __init__(self, width, height):
         self.width = width
@@ -67,38 +83,7 @@ class Maze:
         self.now_map_data = self.map_data[map_num]
 
 
-class Screen:
-    @staticmethod
-    def show_ending_screen(screen, elapsed_time):
-        ending_font = pygame.font.Font(None, 72)
-        message_font = pygame.font.Font(None, 36)
 
-        screen.fill((0, 0, 0))  # 배경을 검은색으로 설정
-        ending_message = ending_font.render("GAME CLEAR!", True, (255, 255, 0))
-        time_message = message_font.render(f"total time: {elapsed_time} sec", True, (255, 255, 255))
-        restart_message = message_font.render("Press 'm' to play again", True, (255, 255, 255))
-
-        screen.blit(ending_message, (screen.get_width() // 2 - ending_message.get_width() // 2, 
-                                      screen.get_height() // 2 - ending_message.get_height() // 2 - 20))
-        screen.blit(time_message, (screen.get_width() // 2 - time_message.get_width() // 2, 
-                                    screen.get_height() // 2 + time_message.get_height() // 2))
-        screen.blit(restart_message, (screen.get_width() // 2 - restart_message.get_width() // 2, 
-                                       screen.get_height() // 2 + time_message.get_height() + 20))
-
-        pygame.mixer.Sound('sound_clear.mp3').play(-1)
-        pygame.display.flip()
-
-        waiting_for_input = True
-        while waiting_for_input:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_m:
-                        waiting_for_input = False  # 게임을 다시 시작하기 위한 플래그
-
-        pygame.quit()  # 이전 게임 종료
 
 
 class Player:
@@ -163,6 +148,122 @@ class Player:
             return False
 
 
+
+
+
+class ScreenManager:
+
+    @staticmethod
+    def show_starting_screen(screen):
+        screen.fill((102, 153, 204))
+        font_ = pygame.font.Font(None, 36)
+        enter_to_start = font_.render('Press Enter To START !', True, (102, 255, 255))
+        screen.blit( enter_to_start,
+                     (screen.get_width() // 2 - enter_to_start.get_width() // 2, 600))
+        pygame.display.flip()
+        running = True
+        
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        print('Enter_key is pushed.')
+                        running = False
+                        break
+                        
+    @staticmethod
+    def perceive_input_key():
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1 or event.key == pygame.K_KP_1:
+                        return '1'
+                    elif event.key == pygame.K_2 or event.key == pygame.K_KP_2:
+                        return '2'
+                    elif event.key == pygame.K_3 or event.key == pygame.K_KP_3:
+                        return '3'
+                    elif event.key == pygame.K_4 or event.key == pygame.K_KP_4:
+                        return '4'
+                    elif event.key == pygame.K_5 or event.key == pygame.K_KP_5:
+                        return '5'
+                    elif event.key == pygame.K_6 or event.key == pygame.K_KP_6:
+                        return '6'
+                    elif event.key == pygame.K_7 or event.key == pygame.K_KP_7:
+                        return '7'
+                    elif event.key == pygame.K_8 or event.key == pygame.K_KP_8:
+                        return '8'
+                    elif event.key == pygame.K_9 or event.key == pygame.K_KP_9:
+                        return '9'
+                    elif event.key == pygame.K_0 or event.key == pygame.K_KP_0:
+                        return '0'
+                    else:
+                        continue
+
+                    
+    def input_maze_size(self, screen, font):
+            message_type = ['horizontal', 'vertical']
+            width_height = []
+            for i in range(2):
+                size = ['_']*2
+                input_message = font.render(f"""Type {message_type[i]} tile amount of maze you'll play!""", True, (102, 255, 255))
+                warning_message = pygame.font.Font(None, 23).render("the size must be two-digit", True, (102, 255, 255))
+                screen.fill((102, 153, 204))
+                screen.blit( input_message,
+                     (screen.get_width() // 2 - input_message.get_width() // 2, 560))
+                screen.blit( warning_message,
+                             (screen.get_width() // 2 - warning_message.get_width() // 2, 580))
+                pygame.display.flip()
+                
+                for j in range(2):
+                    user_input = self.perceive_input_key()
+                    size[j] = user_input
+                    font_ = pygame.font.Font(None, 20)
+                    typed_value = font_.render(f"{''.join(size)}", True, (102, 255, 255))
+                    screen.blit(typed_value,
+                                screen.get_width() // 2 - typed_value.get_width() // 2, 620)
+                    pygame.display.flip()
+                width_height.append(int(''.join(size)))
+            return width_height
+                    
+    def show_set_mapsize_screen(self, screen):
+        screen.fill((102, 153, 204))
+        font_ = pygame.font.Font(None, 36)
+        self.input_maze_size(screen, font_)
+
+        
+
+    @staticmethod
+    def show_ending_screen(screen, elapsed_time):
+        ending_font = pygame.font.Font(None, 72)
+        message_font = pygame.font.Font(None, 36)
+
+        screen.fill((0, 0, 0))  # 배경을 검은색으로 설정
+        ending_message = ending_font.render("GAME CLEAR!", True, (255, 255, 0))
+        time_message = message_font.render(f"total time: {elapsed_time} sec", True, (255, 255, 255))
+        restart_message = message_font.render("Press 'm' to play again", True, (255, 255, 255))
+
+        screen.blit(ending_message, (screen.get_width() // 2 - ending_message.get_width() // 2, 
+                                      screen.get_height() // 2 - ending_message.get_height() // 2 - 20))
+        screen.blit(time_message, (screen.get_width() // 2 - time_message.get_width() // 2, 
+                                    screen.get_height() // 2 + time_message.get_height() // 2))
+        screen.blit(restart_message, (screen.get_width() // 2 - restart_message.get_width() // 2, 
+                                       screen.get_height() // 2 + time_message.get_height() + 20))
+
+        pygame.mixer.Sound('sound_clear.mp3').play(-1)
+        pygame.display.flip()
+
+        waiting_for_input = True
+        while waiting_for_input:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        waiting_for_input = False  # 게임을 다시 시작하기 위한 플래그
+        pygame.quit()  # 이전 게임 종료
+
+
 def restart_game():
     # Pygame 초기화
     pygame.init()
@@ -183,10 +284,11 @@ def restart_game():
     player_image = pygame.transform.scale(player_image, (15, 15))
 
     # 게임 설정
-    print('플레이할 미로 크기를 설정하세요. 전체 타일 개수는 3500개 미만을 추천합니다.')
-    x = int(input('미로의 가로 타일 개수 : '))
-    y = int(input('미로의 세로 타일 개수 : '))
-    maze = Maze(x, y)
+    screen = pygame.display.set_mode((900, 700))
+    screen_manager = ScreenManager()
+    screen_manager.show_starting_screen(screen)
+    screen_manager.show_set_mapsize_screen(screen)
+    
     maze.set_edge_tile_num()
     for i in range(3):
         combined_map = pygame.Surface((15 * maze.width, 15 * maze.height))
